ea5464c9889263d3e33b16fddb3190a235564a30
John Rigler <john@rigler.org>
2019-03-03 20:29:50 -0600

    modified:   README.md 	modified:   issy.bash

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# issy-27389.1
 Requires johnrigler/unspendable
 
 Add this to your bash profile with the appropriate directory:
@@ -7,14 +6,9 @@ Add this to your bash profile with the appropriate directory:
 issy () 
 { 
     alias unspendable="~/unspendable/unspendable.py";
-    . ~/issy/issy.lib
+    . ~/issy/issy.bash
 }
 
-Now you can either use issy as a library or via the extended functions that it provides.  Furthermore, you can either use git to update to the latest version of issy, or hardcode to older versions vy filename extension.  The above is stored as file named in the style of: "$FUNCTION_NAME"-"SUM.SUMSIZE".
+To run, just type 'issy', you will then be given the following functions:
 
-This is a fundamental aspect of how Issy can now integrate into Cryptocurrency.
-
-The number is generated with the following command:
-
-declare -f issy | sum
 
--- issy.bash
+++ issy.bash
@@ -27,15 +27,23 @@ issy.test ()
 alias .test=issy.test
 issy.check () 
 { 
-   :   curl https://dogechain.info/address/DDDDDDDDDDDDDDDDDXXXXXXXXXXXYLzC7d 2> /dev/null | grep --color=auto 'text:'
-   curl https://dogechain.info/address/$1 2> /dev/null > $_ISSY_DIR/$1.$$ | grep --color=auto 'text:'
+	: : issy.check $(.test)
+   : curl https://dogechain.info/address/DDDDDDDDDDDDDDDDDXXXXXXXXXXXYLzC7d 2> /dev/null | grep 'text:'
+   curl https://dogechain.info/address/$1 2> /dev/null > $_ISSY_DIR/$1.$$ | grep 'text:'
+   (
    echo $1
    cat $_ISSY_DIR/$1.$$  | grep -A 1 Received | xargs -n4 | sed 's/</ /g' | sed 's/>/ /g' | awk '{print $7}' 
-   cat $_ISSY_DIR/$1.$$  | grep -A 1 Sent | xargs -n4
-
+   cat $_ISSY_DIR/$1.$$  | grep -A 1 Sent | xargs -n4 | sed 's/</ /g' | sed 's/>/ /g' | awk '{print $7}'
+   ) | xargs -n3
 
 }
 issy.date () 
-{ 
+{  
     date '+%Y%W%w'
+}
+issy.reflection () 
+{ 
+    declare -F | grep issy | while read CMD; do
+        $CMD | egrep '^issy|^    : :';
+    done
 }
